d73ae9bb46c6653f0f1efc407f795c94fd1e9d10
jdolle <1841898+jdolle@users.noreply.github.com>
2025-02-03 09:36:43 -0800

    add linkable ref

--- packages/libraries/linkable-specs/src/__tests__/index.spec.ts
+++ packages/libraries/linkable-specs/src/__tests__/index.spec.ts
@@ -1,79 +1,77 @@
 import { DocumentNode, parse, StringValueNode, visit } from 'graphql';
 import { detectLinkedImplementations, LinkableSpec } from '../index';
 
-describe('index', () => {
-  test('LinkableSpec and detectLinkedImplementations can be used to easily implement linked schema functionality', () => {
-    const sdl = `
-          directive @meta(name: String!, content: String!) on SCHEMA | FIELD
-          directive @metadata_example(eg: String!) on FIELD
-          extend schema
-            @link(url: "https://specs.apollo.dev/link/v1.0")
-            @link(url: "https://specs.graphql-hive.com/metadata/v0.1", import: ["@meta"])
-  
-          type Query {
-            ping: String @meta(name: "owner", content: "hive-console-team")
-            pong: String @metadata__example(eg: "1...2...3... Pong")
+test('LinkableSpec and detectLinkedImplementations can be used to easily implement linked schema functionality', () => {
+  const sdl = `
+        directive @meta(name: String!, content: String!) on SCHEMA | FIELD
+        directive @metadata__example(eg: String!) on FIELD
+        extend schema
+          @link(url: "https://specs.apollo.dev/federation/v2.3")
+          @link(url: "https://specs.graphql-hive.com/metadata/v0.1", import: ["@meta"])
+
+        type Query {
+          ping: String @meta(name: "owner", content: "hive-console-team")
+          pong: String @metadata__example(eg: "1...2...3... Pong")
+        }
+    `;
+
+  const metaSpec = new LinkableSpec('https://specs.graphql-hive.com/metadata', {
+    // The return value could be used to map sdl, collect information, or create a graphql yoga plugin.
+    // In this test, it's used to collect metadata information from the schema.
+    'v0.1': resolveImportName => (typeDefs: DocumentNode) => {
+      const collectedMeta: Record<string, Record<string, string>> = {};
+      const metaName = resolveImportName('@meta');
+      const exampleName = resolveImportName('@example');
+      visit(typeDefs, {
+        FieldDefinition: node => {
+          let metaData: Record<string, string> = {};
+          const fieldName = node.name.value;
+          const meta = node.directives?.find(d => d.name.value === metaName);
+          if (meta) {
+            metaData['name'] =
+              (
+                meta.arguments?.find(a => a.name.value === 'name')?.value as
+                  | StringValueNode
+                  | undefined
+              )?.value ?? '??';
+            metaData['content'] =
+              (
+                meta.arguments?.find(a => a.name.value === 'content')?.value as
+                  | StringValueNode
+                  | undefined
+              )?.value ?? '??';
           }
-      `;
 
-    const metaSpec = new LinkableSpec('https://specs.graphql-hive.com/metadata', {
-      // The return value could be used to map sdl, collect information, or create a graphql yoga plugin.
-      // In this test, it's used to collect metadata information from the schema.
-      'v0.1': resolveImportName => (typeDefs: DocumentNode) => {
-        const collectedMeta: Record<string, Record<string, string>> = {};
-        const metaName = resolveImportName('@meta');
-        const exampleName = resolveImportName('@example');
-        visit(typeDefs, {
-          FieldDefinition: node => {
-            let metaData: Record<string, string> = {};
-            const fieldName = node.name.value;
-            const meta = node.directives?.find(d => d.name.value === metaName);
-            if (meta) {
-              metaData['name'] =
-                (
-                  meta.arguments?.find(a => a.name.value === 'name')?.value as
-                    | StringValueNode
-                    | undefined
-                )?.value ?? '??';
-              metaData['content'] =
-                (
-                  meta.arguments?.find(a => a.name.value === 'content')?.value as
-                    | StringValueNode
-                    | undefined
-                )?.value ?? '??';
-            }
-
-            const example = node.directives?.find(d => d.name.value === exampleName);
-            if (example) {
-              metaData['eg'] =
-                (
-                  example.arguments?.find(a => a.name.value === 'eg')?.value as
-                    | StringValueNode
-                    | undefined
-                )?.value ?? '??';
-            }
-            if (Object.keys(metaData).length) {
-              collectedMeta[fieldName] ??= {};
-              collectedMeta[fieldName] = Object.assign(collectedMeta[fieldName], metaData);
-            }
-            return;
-          },
-        });
-        // collect metadata
-        return `running on v0.1.\nFound metadata: ${JSON.stringify(collectedMeta)}}`;
-      },
-      'v0.2': _resolveImportName => (_typeDefs: DocumentNode) => {
-        // collect metadata
-        return `running on v0.2...`;
-      },
-    });
-    const typeDefs = parse(sdl);
-    const linked = detectLinkedImplementations(typeDefs, [metaSpec]);
-    expect(linked.map(link => link(typeDefs))).toMatchInlineSnapshot(`
-      [
-        running on v0.1.
-      Found metadata: {"ping":{"name":"owner","content":"hive-console-team"},"pong":{"eg":"1...2...3... Pong"}}},
-      ]
-    `);
+          const example = node.directives?.find(d => d.name.value === exampleName);
+          if (example) {
+            metaData['eg'] =
+              (
+                example.arguments?.find(a => a.name.value === 'eg')?.value as
+                  | StringValueNode
+                  | undefined
+              )?.value ?? '??';
+          }
+          if (Object.keys(metaData).length) {
+            collectedMeta[fieldName] ??= {};
+            collectedMeta[fieldName] = Object.assign(collectedMeta[fieldName], metaData);
+          }
+          return;
+        },
+      });
+      // collect metadata
+      return `running on v0.1.\nFound metadata: ${JSON.stringify(collectedMeta)}}`;
+    },
+    'v0.2': _resolveImportName => (_typeDefs: DocumentNode) => {
+      // collect metadata
+      return `running on v0.2...`;
+    },
   });
+  const typeDefs = parse(sdl);
+  const linked = detectLinkedImplementations(typeDefs, [metaSpec]);
+  expect(linked.map(link => link(typeDefs))).toMatchInlineSnapshot(`
+    [
+      running on v0.1.
+    Found metadata: {"ping":{"name":"owner","content":"hive-console-team"},"pong":{"eg":"1...2...3... Pong"}}},
+    ]
+  `);
 });
